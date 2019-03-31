12a34e86a27f504242a024d3b2e9467c7212a77a
Gerson <gerson.01@outlook.pt>
2019-03-31 20:49:22 +0000

    Esquema Terminado do Customer (Clientes) e Movie (Filmes) e iniciado a primeira Migration

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,16 +5,26 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 
+
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!"};
+            var movies = GetMovies();
 
-            return View(movie);
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
