244964b3beed3a09c3d215e4a47593fdffced80f
Gerson <gerson.01@outlook.pt>
2019-04-01 00:32:55 +0000

    Criação da Entidade Movies com novos atributos e a entidade Genres
    Implementado mais retificações nos Controllers Customers e Movies e igualmente para as Views e adicionado a View Details para o Movies

---
+++ Vidly/Migrations/201903312220406_SetNameOfMembershipTypes.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SetNameOfMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET Name = 'Pagar e Ir' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Por mês' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Trimestral' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Anual' WHERE Id = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
